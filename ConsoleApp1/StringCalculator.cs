ec7aa5c4d32240bf9b507eee15d2fa6f054d52fb
Krzysztof Czerwonopolski <83087348+JanPalmer@users.noreply.github.com>
2023-03-10 17:41:22 +0100

    Add files via upload

---
+++ ConsoleApp1/StringCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workshop
+{
+    public class StringCalculator
+    {
+        public static int SumString(String str)
+        {
+            //if (str == String.Empty)
+            //    return 0;
+
+            int result = 0;
+            String[] numbers = str.Split('\n', ',');
+            foreach (String s in numbers)
+            {
+                if (s == "") continue;
+                int parsedNumber = Int32.Parse(s);
+                if (parsedNumber > 1000) continue;
+                if (parsedNumber < 0) throw new ArgumentException();
+                result += parsedNumber;
+            }
+
+
+            return result;
+        }
+    }
+}
