e66764befa19d5ca20c0e73e74a3a2b52200d66b
Krzysztof Czerwonopolski <83087348+JanPalmer@users.noreply.github.com>
2023-03-10 17:45:56 +0100

    commented

--- ConsoleApp1/StringCalculator.cs
+++ ConsoleApp1/StringCalculator.cs
@@ -8,19 +8,19 @@ namespace Workshop
     {
         public static int SumString(String str)
         {
-            //if (str == String.Empty)
+            // if (str == String.Empty)
             //    return 0;
 
             int result = 0;
-            String[] numbers = str.Split('\n', ',');
-            foreach (String s in numbers)
-            {
-                if (s == "") continue;
-                int parsedNumber = Int32.Parse(s);
-                if (parsedNumber > 1000) continue;
-                if (parsedNumber < 0) throw new ArgumentException();
-                result += parsedNumber;
-            }
+            // String[] numbers = str.Split('\n', ',');
+            // foreach (String s in numbers)
+            // {
+            //     if (s == "") continue;
+            //     int parsedNumber = Int32.Parse(s);
+            //     if (parsedNumber > 1000) continue;
+            //     if (parsedNumber < 0) throw new ArgumentException();
+            //     result += parsedNumber;
+            // }
 
 
             return result;
