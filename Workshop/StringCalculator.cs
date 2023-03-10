1779283f71bda5a7f41daae065eba14e71cfee31
Krzysztof Czerwonopolski <83087348+JanPalmer@users.noreply.github.com>
2023-03-10 18:02:16 +0100

    Add files via upload

--- Workshop/StringCalculator.cs
+++ Workshop/StringCalculator.cs
@@ -1,43 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Workshop
 {
     public class StringCalculator
     {
-        public static int SumString(string str)
+        public static int SumString(String str)
         {
             if (str == String.Empty)
-                return 0;
+               return 0;
 
             int result = 0;
-            if (int.TryParse(str, out result))
-            { 
-                if(result < 0) { throw new ArgumentException(); }
-                if (result > 1000) return 0;
-                return result; 
-            }
-
-            char separator;
-            string[] numbers;
-            if (str.Substring(0,2) == "//")
+            String[] numbers = str.Split('\n', ',');
+            foreach (String s in numbers)
             {
-                separator = str[2];
-                numbers = str.Substring(4).Split(',', '\n', separator);
+                if (s == "") continue;
+                int parsedNumber = Int32.Parse(s);
+                if (parsedNumber > 1000) continue;
+                if (parsedNumber < 0) throw new ArgumentException();
+                result += parsedNumber;
             }
-            else numbers = str.Split(',', '\n');
-            foreach (var number in numbers)
-            {
-                int num = int.Parse(number);
-                if (num < 0) { throw new ArgumentException(); }
-                if (num > 1000) num = 0;
-                result += num;
-            }
-                
-
 
 
             return result;
